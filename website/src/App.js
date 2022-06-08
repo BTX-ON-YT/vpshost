42365b9b46c50ace6ca2991be8c5301081bbe858
BTX_ON_YT <90880784+BTX-ON-YT@users.noreply.github.com>
2022-06-08 02:54:50 +0000

    1

---
+++ website/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <span className="App-hedder-link"></span>
+      </header>
+      </div>
+  );
+}
+
+export default App;
